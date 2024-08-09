6126ba9cb8579fedf18be1bcb672650720fa2b5e
chandan6893 <krc6893@gmail.com>
2024-08-09 13:06:55 +0530

    convertTo24HrsFormat

--- server/app.js
+++ server/app.js
@@ -327,16 +327,35 @@ num2 = 10;
 
 // console.log(str.replaceAll(" ", "_"));
 
+// **************************************************************************************************************************************
+// *******************************************************************************************************************************************************************************************************************
+// (#8.) Write a function which can convert the time input given in 12 hours format to 24 hours format
+// Example
+
+
+// convertTo24HrsFormat("12:10AM"); // 00:10
+// convertTo24HrsFormat("5:00AM"); // 05:00
+// convertTo24HrsFormat("12:33PM"); // 12:33
+// convertTo24HrsFormat("01:59PM"); // 13:59
+// convertTo24HrsFormat("11:8PM"); // 23:08
+// convertTo24HrsFormat("10:02PM"); // 22:02
+  
+function convertTo24HrsFormat(timeText) {
+    var timeTextLower = timeText.toLowerCase();
+    let [hours, mins] = timeTextLower.split(":");
+   
+    // 12 o clock is the special case to be handled both for AM and PM
+    if (timeTextLower.endsWith("am")) hours = hours == 12 ? "0" : hours;
+    else if (timeTextLower.endsWith("pm"))
+      hours = hours == 12 ? hours : String(+hours + 12);
+     
+    return hours.padStart(2, 0) + ":" + mins.slice(0, -2).padStart(2, 0);
+  }
+
+console.log(convertTo24HrsFormat("01:10PM"))
 
 
 
-
-
-
-const str = "submit button";
-// console.log(str.split(" ").join("_"));
-
-console.log(str.replaceAll(" ","_"))
 
 
 
