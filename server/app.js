b4bca4334c98b2ea17f8e36e328f80cd2b6f163f
chandan6893 <krc6893@gmail.com>
2024-08-09 12:02:49 +0530

    ReplaceAll

--- server/app.js
+++ server/app.js
@@ -316,3 +316,34 @@ num2 = 10;
 
 // ***************************************************************************************************************************
 
+
+// ( #7 )Write a code to replace all the spaces of the string with underscores
+
+// The string can be split using the space character and can be joined back with underscore to replace all the spaces with strings
+// replaceAll is the inbuilt String function on prototype which can be used to replace a string with another string
+
+// const str = "submit button"
+// console.log(str.split(" ").join("_"))
+
+// console.log(str.replaceAll(" ", "_"));
+
+
+
+
+
+
+
+const str = "submit button";
+// console.log(str.split(" ").join("_"));
+
+console.log(str.replaceAll(" ","_"))
+
+
+
+
+
+
+
+
+
+
